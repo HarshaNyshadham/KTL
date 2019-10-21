fc6b0ac3ec1fefd96df66fda3a4a0c3c482f27ca
Harshanand Nyshadham <harsha.nyshadham@gmail.com>
2019-10-21 09:17:38 -0500

    csrf

--- source/__init__.py
+++ source/__init__.py
@@ -15,8 +15,11 @@ db = SQLAlchemy(app)
 login = LoginManager(app)
 admin=Admin(app)
 mail=Mail(app)
-# csrf = CsrfProtect()
-# csrf.init_app(app)
+#csrf = CsrfProtect()
+#csrf.init_app(app)
+csrf = CsrfProtect()
+csrf.init_app(app)
+WTF_CSRF_CHECK_DEFAULT = False
 
 
 

bcc5d5aaceed7a0a8e8a36255d8a12b5996c58ea
Harshanand Nyshadham <harsha.nyshadham@gmail.com>
2021-06-15 11:35:48 -0500

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config(object):
     databasename="katytennisleague$default",
 )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.urandom(32)
     SECRET_KEY = os.environ.get('SECRET_KEY') # or 'iam-simple'
     SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_TRACK_MODIFICATIONS = False

3a853db06626cc588afb0998f4878e5a1146a0d5
Harshanand Nyshadham <harsha.nyshadham@gmail.com>
2021-06-29 13:58:55 -0500

    Update routes.py

--- source/routes.py
+++ source/routes.py
@@ -53,7 +53,7 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
 
-        login_user(userLogged, remember=form.remember_me.data)
+        print(login_user(userLogged, remember=form.remember_me.data))
         print(current_user)
         if(userLogged.firstName in FVL_PlayedId):
           return redirect(url_for('FVLindex'))

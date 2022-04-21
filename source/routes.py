5ec0eab42b6efb87d1050bf4af58a64624efdede
Harshanand Nyshadham <harsha.nyshadham@gmail.com>
2022-04-21 12:41:31 -0500

    Update routes.py

--- source/routes.py
+++ source/routes.py
@@ -802,11 +802,11 @@ def update_points_doubles(filename,p1,p1points,p2,p2points,winner,bonusplayer,ga
   print(gamest1,gamest2)
   if(gamest1>0):
     print(df.at[p1index,'GamesWon'],df.at[p1index,'GamesTotal'])
-    df.at[p1index,'%games']=float(df.at[p1index,'GamesWon']/df.at[p1index,'GamesTotal'])*100
+    df.at[p1index,'%games']=float(df.at[p1index,'GamesWon'])/float(df.at[p1index,'GamesTotal'])*100
     print(df.at[p1index,'%games'],(df.at[p1index,'GamesWon']/df.at[p1index,'GamesTotal'])*100)
   df.at[p2index,'GamesWon']=df.at[p2index,'GamesWon']+gamest2
   if(gamest2>0):
-    df.at[p2index,'%games']=float(df.at[p2index,'GamesWon']/df.at[p2index,'GamesTotal'])*100
+    df.at[p2index,'%games']=float(df.at[p2index,'GamesWon'])/float(df.at[p2index,'GamesTotal'])*100
 
   print(df.at[p1index,'%games'],df.at[p2index,'%games'])
 
